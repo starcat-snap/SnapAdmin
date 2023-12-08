722c04475e01e4645c8b44b5899951a3cb06badd
wang <gqsforme@gmail.com>
2023-12-09 00:42:58 +0800

    Remove core deprecations

--- packages/Core/System/Language/LanguageDefinition.php
+++ packages/Core/System/Language/LanguageDefinition.php
@@ -9,19 +9,13 @@ use SnapAdmin\Core\Framework\DataAbstractionLayer\Field\FkField;
 use SnapAdmin\Core\Framework\DataAbstractionLayer\Field\Flag\ApiAware;
 use SnapAdmin\Core\Framework\DataAbstractionLayer\Field\Flag\PrimaryKey;
 use SnapAdmin\Core\Framework\DataAbstractionLayer\Field\Flag\Required;
-use SnapAdmin\Core\Framework\DataAbstractionLayer\Field\Flag\RestrictDelete;
 use SnapAdmin\Core\Framework\DataAbstractionLayer\Field\IdField;
-use SnapAdmin\Core\Framework\DataAbstractionLayer\Field\ManyToManyAssociationField;
 use SnapAdmin\Core\Framework\DataAbstractionLayer\Field\ManyToOneAssociationField;
-use SnapAdmin\Core\Framework\DataAbstractionLayer\Field\OneToManyAssociationField;
 use SnapAdmin\Core\Framework\DataAbstractionLayer\Field\ParentAssociationField;
 use SnapAdmin\Core\Framework\DataAbstractionLayer\Field\ParentFkField;
 use SnapAdmin\Core\Framework\DataAbstractionLayer\Field\StringField;
 use SnapAdmin\Core\Framework\DataAbstractionLayer\FieldCollection;
 use SnapAdmin\Core\System\Locale\LocaleDefinition;
-use Storefront\System\SalesChannel\Aggregate\SalesChannelDomain\SalesChannelDomainDefinition;
-use Storefront\System\SalesChannel\Aggregate\SalesChannelLanguage\SalesChannelLanguageDefinition;
-use Storefront\System\SalesChannel\SalesChannelDefinition;
 
 
 class LanguageDefinition extends EntityDefinition
@@ -55,9 +49,6 @@ class LanguageDefinition extends EntityDefinition
             (new ParentFkField(self::class))->addFlags(new ApiAware()),
             (new FkField('locale_id', 'localeId', LocaleDefinition::class))->addFlags(new ApiAware(), new Required()),
             (new FkField('translation_code_id', 'translationCodeId', LocaleDefinition::class))->addFlags(new ApiAware()),
-//            (new OneToManyAssociationField('salesChannelDomains', SalesChannelDomainDefinition::class, 'language_id'))->addFlags(new RestrictDelete()),
-//            new ManyToManyAssociationField('salesChannels', SalesChannelDefinition::class, SalesChannelLanguageDefinition::class, 'language_id', 'sales_channel_id'),
-
             (new StringField('name', 'name'))->addFlags(new ApiAware(), new Required()),
             (new CustomFields())->addFlags(new ApiAware()),
             (new ParentAssociationField(self::class, 'id'))->addFlags(new ApiAware()),
