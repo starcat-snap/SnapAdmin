f402639b6c1902e1af3eac0737c824ae37d32566
wang <lyricsong47@gmail.com>
2023-12-11 13:17:30 +0800

    Fix php stan not working

--- packages/Core/Framework/DependencyInjection/Configuration.php
+++ packages/Core/Framework/DependencyInjection/Configuration.php
@@ -21,31 +21,152 @@ class Configuration implements ConfigurationInterface
             ->append($this->createHttpCacheSection())
             ->append($this->createProfilerSection())
             ->append($this->createFilesystemSection())
-            ->append($this->createCdnSection())
             ->append($this->createApiSection())
             ->append($this->createStoreSection())
-            ->append($this->createCartSection())
             ->append($this->createAdminWorkerSection())
             ->append($this->createAutoUpdateSection())
-            ->append($this->createSitemapSection())
             ->append($this->createDeploymentSection())
             ->append($this->createMediaSection())
             ->append($this->createDalSection())
             ->append($this->createMailSection())
             ->append($this->createFeatureSection())
             ->append($this->createLoggerSection())
-            ->append($this->createCacheSection())
-            ->append($this->createHtmlSanitizerSection())
             ->append($this->createIncrementSection())
             ->append($this->createTwigSection())
+            ->append($this->createCacheSection())
             ->append($this->createDompdfSection())
-            ->append($this->createStockSection())
-            ->append($this->createUsageDataSection())
+            ->append($this->createCdnSection())
+            ->append($this->createHtmlSanitizerSection())
             ->end();
 
         return $treeBuilder;
     }
+    private function createHtmlSanitizerSection(): ArrayNodeDefinition
+    {
+        $treeBuilder = new TreeBuilder('html_sanitizer');
 
+        $rootNode = $treeBuilder->getRootNode();
+        $rootNode
+            ->children()
+                ->booleanNode('enabled')
+                    ->defaultTrue()
+                ->end()
+                ->variableNode('cache_dir')
+                    ->defaultValue('%kernel.cache_dir%')
+                ->end()
+                ->booleanNode('cache_enabled')
+                    ->defaultTrue()
+                ->end()
+                ->arrayNode('sets')
+                    ->useAttributeAsKey('name')
+                    ->arrayPrototype()
+                        ->children()
+                            ->scalarNode('name')->end()
+                            ->arrayNode('tags')
+                                ->defaultValue([])
+                                ->scalarPrototype()->end()
+                            ->end()
+                            ->arrayNode('attributes')
+                                ->defaultValue([])
+                                ->scalarPrototype()->end()
+                            ->end()
+                            ->arrayNode('custom_attributes')
+                                ->defaultValue([])
+                                ->arrayPrototype()
+                                    ->children()
+                                        ->arrayNode('tags')
+                                            ->defaultValue([])
+                                            ->scalarPrototype()->end()
+                                        ->end()
+                                        ->arrayNode('attributes')
+                                            ->defaultValue([])
+                                            ->scalarPrototype()->end()
+                                        ->end()
+                                    ->end()
+                                ->end()
+                            ->end()
+                            ->arrayNode('options')
+                                ->useAttributeAsKey('key')
+                                ->defaultValue([])
+                                ->arrayPrototype()
+                                    ->children()
+                                        ->scalarNode('key')->end()
+                                        ->scalarNode('value')->end()
+                                        ->arrayNode('values')
+                                            ->defaultValue([])
+                                            ->scalarPrototype()->end()
+                                        ->end()
+                                    ->end()
+                                ->end()
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end()
+                ->arrayNode('fields')
+                    ->useAttributeAsKey('name')
+                    ->arrayPrototype()
+                        ->children()
+                            ->scalarNode('name')->end()
+                            ->arrayNode('sets')
+                                ->scalarPrototype()->end()
+                            ->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end();
+
+        return $rootNode;
+    }
+    private function createCdnSection(): ArrayNodeDefinition
+    {
+        $rootNode = (new TreeBuilder('cdn'))->getRootNode();
+        $rootNode
+            ->children()
+                ->scalarNode('url')->end()
+                ->scalarNode('strategy')->end()
+            ->end();
+
+        return $rootNode;
+    }
+    private function createCacheSection(): ArrayNodeDefinition
+    {
+        $rootNode = (new TreeBuilder('cache'))->getRootNode();
+        $rootNode
+            ->children()
+                ->scalarNode('redis_prefix')->end()
+                ->booleanNode('cache_compression')->defaultTrue()->end()
+                ->arrayNode('tagging')
+                    ->children()
+                        ->booleanNode('each_snippet')
+                            ->defaultTrue()
+                        ->end()
+                        ->booleanNode('each_config')
+                            ->defaultTrue()
+                        ->end()
+                        ->booleanNode('each_theme_config')
+                            ->defaultTrue()
+                        ->end()
+                    ->end()
+                ->end()
+                ->arrayNode('invalidation')
+                    ->children()
+                        ->integerNode('delay')
+                            ->defaultValue(0)
+                        ->end()
+                        ->arrayNode('delay_options')
+                            ->children()
+                                ->scalarNode('storage')
+                                    ->defaultValue('redis')
+                                ->end()
+                                ->scalarNode('dsn')
+                                    ->defaultValue('redis://localhost')
+                                ->end()
+                            ->end()
+                        ->end()
+                    ->end()
+            ->end();
+        return $rootNode;
+    }
     private function createFilesystemSection(): ArrayNodeDefinition
     {
         $rootNode = (new TreeBuilder('filesystem'))->getRootNode();
@@ -101,17 +222,6 @@ class Configuration implements ConfigurationInterface
         return $rootNode;
     }
 
-    private function createCdnSection(): ArrayNodeDefinition
-    {
-        $rootNode = (new TreeBuilder('cdn'))->getRootNode();
-        $rootNode
-            ->children()
-            ->scalarNode('url')->end()
-            ->scalarNode('strategy')->end()
-            ->end();
-
-        return $rootNode;
-    }
 
     private function createApiSection(): ArrayNodeDefinition
     {
@@ -215,51 +325,6 @@ class Configuration implements ConfigurationInterface
         return $rootNode;
     }
 
-    private function createSitemapSection(): ArrayNodeDefinition
-    {
-        $treeBuilder = new TreeBuilder('sitemap');
-
-        $rootNode = $treeBuilder->getRootNode();
-        $rootNode
-            ->children()
-            ->arrayNode('custom_urls')
-            ->arrayPrototype()
-            ->children()
-            ->scalarNode('url')->end()
-            ->scalarNode('lastMod')->end()
-            ->enumNode('changeFreq')
-            ->values([
-                'always',
-                'hourly',
-                'daily',
-                'weekly',
-                'monthly',
-                'yearly',
-            ])
-            ->end()
-            ->floatNode('priority')->end()
-            ->scalarNode('channelId')->end()
-            ->end()
-            ->end()
-            ->end()
-            ->arrayNode('excluded_urls')
-            ->arrayPrototype()
-            ->children()
-            ->scalarNode('resource')->end()
-            ->scalarNode('identifier')->end()
-            ->scalarNode('channelId')->end()
-            ->end()
-            ->end()
-            ->end()
-            ->integerNode('batchsize')
-            ->min(1)
-            ->defaultValue(100)
-            ->end()
-            ->end();
-
-        return $rootNode;
-    }
-
     private function createDeploymentSection(): ArrayNodeDefinition
     {
         $rootNode = (new TreeBuilder('deployment'))->getRootNode();
@@ -345,124 +410,6 @@ class Configuration implements ConfigurationInterface
         return $rootNode;
     }
 
-    private function createCacheSection(): ArrayNodeDefinition
-    {
-        $rootNode = (new TreeBuilder('cache'))->getRootNode();
-        $rootNode
-            ->children()
-            ->scalarNode('redis_prefix')->end()
-            ->booleanNode('cache_compression')->defaultTrue()->end()
-            ->arrayNode('tagging')
-            ->children()
-            ->booleanNode('each_snippet')
-            ->defaultTrue()
-            ->end()
-            ->booleanNode('each_config')
-            ->defaultTrue()
-            ->end()
-            ->booleanNode('each_theme_config')
-            ->defaultTrue()
-            ->end()
-            ->end()
-            ->end()
-            ->arrayNode('invalidation')
-            ->children()
-            ->integerNode('delay')
-            ->defaultValue(0)
-            ->end()
-            ->arrayNode('delay_options')
-            ->children()
-            ->scalarNode('storage')
-            ->defaultValue('cache')
-            ->end()
-            ->scalarNode('dsn')
-            ->defaultValue('redis://localhost')
-            ->end()
-            ->end()
-            ->end()
-            ->integerNode('count')
-            ->setDeprecated('snap/platform', '6.6.0.0')
-            ->defaultValue(150)
-            ->end()
-            ->arrayNode('http_cache')
-            ->performNoDeepMerging()
-            ->prototype('scalar')->end()
-            ->end()
-            ->arrayNode('product_listing_route')
-            ->performNoDeepMerging()
-            ->prototype('scalar')->end()
-            ->end()
-            ->arrayNode('product_detail_route')
-            ->performNoDeepMerging()
-            ->prototype('scalar')->end()
-            ->end()
-            ->arrayNode('product_search_route')
-            ->performNoDeepMerging()
-            ->prototype('scalar')->end()
-            ->end()
-            ->arrayNode('product_suggest_route')
-            ->performNoDeepMerging()
-            ->prototype('scalar')->end()
-            ->end()
-            ->arrayNode('product_cross_selling_route')
-            ->performNoDeepMerging()
-            ->prototype('scalar')->end()
-            ->end()
-            ->arrayNode('payment_method_route')
-            ->performNoDeepMerging()
-            ->prototype('scalar')->end()
-            ->end()
-            ->arrayNode('shipping_method_route')
-            ->performNoDeepMerging()
-            ->prototype('scalar')->end()
-            ->end()
-            ->arrayNode('navigation_route')
-            ->performNoDeepMerging()
-            ->prototype('scalar')->end()
-            ->end()
-            ->arrayNode('category_route')
-            ->performNoDeepMerging()
-            ->prototype('scalar')->end()
-            ->end()
-            ->arrayNode('landing_page_route')
-            ->performNoDeepMerging()
-            ->prototype('scalar')->end()
-            ->end()
-            ->arrayNode('language_route')
-            ->performNoDeepMerging()
-            ->prototype('scalar')->end()
-            ->end()
-            ->arrayNode('currency_route')
-            ->performNoDeepMerging()
-            ->prototype('scalar')->end()
-            ->end()
-            ->arrayNode('country_route')
-            ->performNoDeepMerging()
-            ->prototype('scalar')->end()
-            ->end()
-            ->arrayNode('country_state_route')
-            ->performNoDeepMerging()
-            ->prototype('scalar')->end()
-            ->end()
-            ->arrayNode('salutation_route')
-            ->performNoDeepMerging()
-            ->prototype('scalar')->end()
-            ->end()
-            ->arrayNode('product_review_route')
-            ->performNoDeepMerging()
-            ->prototype('scalar')->end()
-            ->end()
-            ->arrayNode('sitemap_route')
-            ->performNoDeepMerging()
-            ->prototype('scalar')->end()
-            ->end()
-            ->end()
-            ->end()
-            ->end();
-
-        return $rootNode;
-    }
-
     private function createDalSection(): ArrayNodeDefinition
     {
         $treeBuilder = new TreeBuilder('dal');
@@ -483,101 +430,6 @@ class Configuration implements ConfigurationInterface
             ->integerNode('expire_days')
             ->min(1)
             ->defaultValue(30)
-            ->end()
-            ->end()
-            ->end();
-
-        return $rootNode;
-    }
-
-    private function createCartSection(): ArrayNodeDefinition
-    {
-        $treeBuilder = new TreeBuilder('cart');
-
-        $rootNode = $treeBuilder->getRootNode();
-        $rootNode
-            ->children()
-            ->booleanNode('compress')->defaultFalse()->end()
-            ->integerNode('expire_days')
-            ->min(1)
-            ->defaultValue(120)
-            ->end()
-            ->scalarNode('redis_url')->end()
-            ->end();
-
-        return $rootNode;
-    }
-
-    private function createHtmlSanitizerSection(): ArrayNodeDefinition
-    {
-        $treeBuilder = new TreeBuilder('html_sanitizer');
-
-        $rootNode = $treeBuilder->getRootNode();
-        $rootNode
-            ->children()
-            ->booleanNode('enabled')
-            ->defaultTrue()
-            ->end()
-            ->variableNode('cache_dir')
-            ->defaultValue('%kernel.cache_dir%')
-            ->end()
-            ->booleanNode('cache_enabled')
-            ->defaultTrue()
-            ->end()
-            ->arrayNode('sets')
-            ->useAttributeAsKey('name')
-            ->arrayPrototype()
-            ->children()
-            ->scalarNode('name')->end()
-            ->arrayNode('tags')
-            ->defaultValue([])
-            ->scalarPrototype()->end()
-            ->end()
-            ->arrayNode('attributes')
-            ->defaultValue([])
-            ->scalarPrototype()->end()
-            ->end()
-            ->arrayNode('custom_attributes')
-            ->defaultValue([])
-            ->arrayPrototype()
-            ->children()
-            ->arrayNode('tags')
-            ->defaultValue([])
-            ->scalarPrototype()->end()
-            ->end()
-            ->arrayNode('attributes')
-            ->defaultValue([])
-            ->scalarPrototype()->end()
-            ->end()
-            ->end()
-            ->end()
-            ->end()
-            ->arrayNode('options')
-            ->useAttributeAsKey('key')
-            ->defaultValue([])
-            ->arrayPrototype()
-            ->children()
-            ->scalarNode('key')->end()
-            ->scalarNode('value')->end()
-            ->arrayNode('values')
-            ->defaultValue([])
-            ->scalarPrototype()->end()
-            ->end()
-            ->end()
-            ->end()
-            ->end()
-            ->end()
-            ->end()
-            ->end()
-            ->arrayNode('fields')
-            ->useAttributeAsKey('name')
-            ->arrayPrototype()
-            ->children()
-            ->scalarNode('name')->end()
-            ->arrayNode('sets')
-            ->scalarPrototype()->end()
-            ->end()
-            ->end()
             ->end()
             ->end()
             ->end();
@@ -655,37 +507,6 @@ class Configuration implements ConfigurationInterface
             ->arrayNode('options')
             ->useAttributeAsKey('name')
             ->scalarPrototype()
-            ->end()
-            ->end()
-            ->end();
-
-        return $rootNode;
-    }
-
-    private function createStockSection(): ArrayNodeDefinition
-    {
-        $treeBuilder = new TreeBuilder('stock');
-
-        $rootNode = $treeBuilder->getRootNode();
-        $rootNode
-            ->children()
-            ->booleanNode('enable_stock_management')->defaultTrue()->end()
-            ->end();
-
-        return $rootNode;
-    }
-
-    private function createUsageDataSection(): ArrayNodeDefinition
-    {
-        $treeBuilder = new TreeBuilder('usage_data');
-
-        $rootNode = $treeBuilder->getRootNode();
-        $rootNode
-            ->children()
-            ->arrayNode('gateway')
-            ->children()
-            ->scalarNode('base_uri')->end()
-            ->scalarNode('batch_size')->end()
             ->end()
             ->end()
             ->end();
