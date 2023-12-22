f4f32cd84d0a13c4fcc72b746a39311d793260a1
wang <lyricsong47@gmail.com>
2023-12-22 12:51:52 +0800

    Create command to generate unit and migration tests

--- packages/Core/DevOps/DevOps.php
+++ packages/Core/DevOps/DevOps.php
@@ -28,5 +28,8 @@ class DevOps extends Bundle
         if ($environment === 'e2e') {
             $loader->load('services_e2e.xml');
         }
+        if ($environment === 'dev') {
+            $loader->load('services_dev.xml');
+        }
     }
 }
