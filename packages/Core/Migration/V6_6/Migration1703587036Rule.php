197411bde5dd5de00f7177d82a2be93bd817778e
wang <lyricsong47@gmail.com>
2024-01-07 21:57:14 +0800

    Add field to rule table

--- packages/Core/Migration/V6_6/Migration1703587036Rule.php
+++ packages/Core/Migration/V6_6/Migration1703587036Rule.php
@@ -21,19 +21,20 @@ class Migration1703587036Rule extends MigrationStep
     {
         $connection->executeStatement('
             CREATE TABLE `rule` (
-              `id`              BINARY(16)      NOT NULL,
-              `name`            VARCHAR(500)    NOT NULL,
-              `description`     LONGTEXT        NULL,
-              `priority`        INT(11)         NOT NULL,
-              `payload`         LONGBLOB        NULL,
-              `invalid`         TINYINT(1)      NOT NULL DEFAULT 0,
-              `module_types`    JSON            NULL,
-              `custom_fields`   JSON            NULL,
-              `created_at`      DATETIME(3)     NOT NULL,
-              `updated_at`      DATETIME(3)     NULL,
+              `id` binary(16) NOT NULL,
+              `name` varchar(500) NOT NULL,
+              `description` longtext DEFAULT NULL,
+              `priority` int(11) NOT NULL,
+              `payload` longblob DEFAULT NULL,
+              `invalid` tinyint(1) NOT NULL DEFAULT 0,
+              `areas` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`areas`)),
+              `module_types` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`module_types`)),
+              `custom_fields` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`custom_fields`)),
+              `created_at` datetime(3) NOT NULL,
+              `updated_at` datetime(3) DEFAULT NULL,
               PRIMARY KEY (`id`),
-              CONSTRAINT `json.rule.module_types` CHECK (JSON_VALID(`module_types`)),
-              CONSTRAINT `json.rule.custom_fields` CHECK (JSON_VALID(`custom_fields`))
+              CONSTRAINT `json.rule.module_types` CHECK (json_valid(`module_types`)),
+              CONSTRAINT `json.rule.custom_fields` CHECK (json_valid(`custom_fields`))
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
         ');
     }
